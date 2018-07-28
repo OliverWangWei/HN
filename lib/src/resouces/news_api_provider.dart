d369cd15726233cee2e4f6a1dc208fe14b89dd7e
王威 <wangwei@MacBook-Pro-2.local>
2018-07-28 22:14:52 +0800

    first commit

---
+++ lib/src/resouces/news_api_provider.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import '../models/item_model.dart';
+
+final _root = 'https://hacker-news.firebaseiox.com/v0';
+
+class NewsApiProvider {
+  Client client = Client();
+
+  fetchTopIds() async {
+    final response = await client.get('$_root/topstories.json');
+    final ids = json.decode(response.body);
+
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id.json');
+    final parsedJson = json.decode(response.body);
+
+    // make the instance of ItemModel and return it
+    return ItemModel.fromJson(parsedJson);
+  }
+}
---
+++ test/src/resouces/news_api_provider_test.dart
@@ -0,0 +1,44 @@
+import 'package:news/src/resouces/news_api_provider.dart';
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds returns a list of ids', () async {
+    // set up the test case
+    final newsApi = NewsApiProvider();
+
+    // when client get is excuted rather than making he real request
+    // the inner function will be invoked
+    // and it return to response in news_api_provider.dart
+
+    // the moment async function is called it will return future
+    newsApi.client = MockClient((request) async {
+      // the response
+      return Response(json.encode([1, 2, 3]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
+
+    // expectation   (actual, matcher)
+
+    expect(ids, [1, 2, 3]);
+
+  });
+
+  test('FetchItem returns item model', () async {
+
+    final newsApi = NewsApiProvider();
+
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+    final item = await newsApi.fetchItem(999);
+
+    expect(item.id, 123);
+
+  });
+
+}
