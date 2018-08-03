444ae7999da0e5ed11e513b6ec06c1a702ceb189
OliverWangWei <a0924100192@gmail.com>
2018-08-03 22:41:05 +0800

    constucting UI layout

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import '../models/item_model.dart';
 import '../blocs/comments_provider.dart';
 
 class NewsDetail extends StatelessWidget {
@@ -16,13 +19,45 @@ class NewsDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text('News Detial'),
       ),
-      body: Center(
-        child: buildBody(bloc),
-      ),
+      body: buildBody(bloc),
     );
   }
 
   Widget buildBody(CommentsBloc bloc) {
-    
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading');
+        }
+
+        return FutureBuilder(
+          future: snapshot.data[itemId],
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Loading');
+            }
+
+            return buildTitle(itemSnapshot.data);
+
+          },
+        );
+
+      },
+    );
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
   }
 }
